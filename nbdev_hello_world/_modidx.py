ee0fab44f2a94af67486ef66ee8b00c351ade4f8
victorbahlangene <victorbahlangene@outlook.com>
2022-12-22 17:07:45 +0000

    Add `say_hello`; update index

--- nbdev_hello_world/_modidx.py
+++ nbdev_hello_world/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://victorbahlangene.github.io',
                 'git_url': 'https://github.com/victorbahlangene/nbdev-hello-world',
                 'lib_path': 'nbdev_hello_world'},
-  'syms': {'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')}}}
+  'syms': { 'nbdev_hello_world.core': { 'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py'),
+                                        'nbdev_hello_world.core.say_hello': ('core.html#say_hello', 'nbdev_hello_world/core.py')}}}
